bb932ddb9f8833b6c6d45678195d668f4b5045fd
АЛЕКСАНДР СТОЙКО <stoiko.sasha2003@gmail.com>
2025-03-22 21:19:51 +0700

    Added loadCities() and getCityCount() methods

--- Game_Cities.h
+++ Game_Cities.h
@@ -1,7 +1,33 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
 class Game_Cities
 {
+private:
+    vector<string> cities;
 public:
-	Game_Cities() {};
+    Game_Cities() {};
+
+    void loadCities(const string& filename) {
+        ifstream file(filename);
+        if (!file) {
+            cout << "Ошибка открытия файла: " << filename << endl;
+            return;
+        }
+        string city;
+        while (getline(file, city)) {
+            if (!city.empty()) {
+                cities.push_back(city);
+            }
+        }
+        file.close();
+    }
+    size_t getCityCount() const { return cities.size(); }
 };
 
